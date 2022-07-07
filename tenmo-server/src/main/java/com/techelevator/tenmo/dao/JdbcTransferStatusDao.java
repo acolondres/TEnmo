1512afa7621e7421ee395a60b223290d7d10ba30
BROOKE <LILRODGERS58@GMAIL.COM>
2022-07-07 13:42:14 -0400

    capstone

--- tenmo-server/src/main/java/com/techelevator/tenmo/dao/JdbcTransferStatusDao.java
+++ tenmo-server/src/main/java/com/techelevator/tenmo/dao/JdbcTransferStatusDao.java
@@ -1,17 +1,42 @@
 package com.techelevator.tenmo.dao;
-
+import org.springframework.jdbc.support.rowset.SqlRowSet;
 import com.techelevator.tenmo.model.TransferStatus;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.rowset.SqlRowSet;
 import org.springframework.stereotype.Component;
 
 @Component
 public class JdbcTransferStatusDao implements TransferStatusDao {
+    private JdbcTemplate jdbcTemplate;
+    public JdbcTransferStatusDao(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
     @Override
     public TransferStatus getTransferStatusById(int transferStatusId) {
-        return null;
+        String sql = " SELECT transfer_status_id, transfer_status_desc"+
+                " FROM transfer_status"+
+                " WHERE transfer_status_id = ?";
+        SqlRowSet results = jdbcTemplate.queryForRowSet(sql,transferStatusId);
+        TransferStatus transferStatus = null;
+        if(results.next()){
+            int transferStatusId2 = results.getInt("transfer_status_id");
+            String transferStatusInfo = results.getString("transfer_status_desc");
+            transferStatus = new TransferStatus(transferStatusId2, transferStatusInfo);
+        }return transferStatus;
     }
 
     @Override
     public TransferStatus getTransferStatusByInfo(String info) {
-        return null;
+        String sql = " SELECT transfer_status_id, transfer_status_desc"+
+                " FROM transfer_status"+
+                " WHERE transfer_status_desc = ?";
+        SqlRowSet results = jdbcTemplate.queryForRowSet(sql,info);
+        TransferStatus transferStatus = null;
+        if(results.next()){
+            int transferStatusId = results.getInt("transfer_status_id");
+            String transferStatusInfo = results.getString("transfer_status_desc");
+            transferStatus = new TransferStatus(transferStatusId, transferStatusInfo);
+        }return transferStatus;
     }
 }
--- tenmo-server/src/main/java/com/techelevator/tenmo/dao/JdbcTransferTypeDao.java
+++ tenmo-server/src/main/java/com/techelevator/tenmo/dao/JdbcTransferTypeDao.java
@@ -1,15 +1,46 @@
 package com.techelevator.tenmo.dao;
 
 import com.techelevator.tenmo.model.TransferType;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Component;
+import org.springframework.jdbc.support.rowset.SqlRowSet;
 
+@Component
 public class JdbcTransferTypeDao implements TransferTypeDao {
+    private JdbcTemplate jdbcTemplate;
+    public JdbcTransferTypeDao(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
     @Override
     public TransferType getTransferTypeFromId(int transferId) {
-        return null;
+        String sql = " SELECT transfer_type_id, transfer_type_desc"+
+                " FROM transfer_type"+
+                " WHERE transfer_type_id = ?";
+        SqlRowSet results = jdbcTemplate.queryForRowSet(sql,transferId);
+        TransferType transferType = null;
+        if(results.next()){
+            int transferTypeId = results.getInt("transfer_type_id");
+            String transferTypeInfo = results.getString("transfer_type_desc");
+            transferType = new TransferType(transferTypeId,transferTypeInfo);
+        }
+
+        return transferType;
     }
 
     @Override
     public TransferType getTransferTypeByInfo(String info) {
-        return null;
+        String sql = " SELECT transfer_type_id, transfer_type_desc"+
+                " FROM transfer_type"+
+                " WHERE transfer_type_desc = ?";
+        SqlRowSet results = jdbcTemplate.queryForRowSet(sql,info);
+        TransferType transferType = null;
+        if(results.next()){
+            int transferTypeId = results.getInt("transfer_type_id");
+            String transferTypeInfo = results.getString("transfer_type_desc");
+            transferType = new TransferType(transferTypeId,transferTypeInfo);
+        }
+
+        return transferType;
     }
 }
